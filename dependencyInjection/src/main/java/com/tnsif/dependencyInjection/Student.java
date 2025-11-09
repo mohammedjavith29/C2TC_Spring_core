cc4f337881d23643600be3832a08354f86cdd259
lenovo <lenovo@MR-JAVITH>
2025-11-09 21:46:11 +0530

    Dependency Injection Completed

---
+++ dependencyInjection/src/main/java/com/tnsif/dependencyInjection/Student.java
@@ -0,0 +1,28 @@
+
+package com.tnsif.dependencyInjection;
+
+public class Student {
+	int sid;
+	String sname;
+	String city;
+	
+	
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+
+	
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public void showInfo()
+	{
+		System.out.println("Id:"+sid+" Name:"+sname+" City:"+city);
+	}
+}
